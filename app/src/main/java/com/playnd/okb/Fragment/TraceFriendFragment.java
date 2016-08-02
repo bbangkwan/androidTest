5fcfcb9408dbe566e3185a4b89d214ac222a70d1
bbangkwan <bbangkwan@gmail.com>
2016-08-02 18:06:56 +0900

    [20160801] - 환율 계산 레이아웃 - google map api 연동

--- app/src/main/java/com/playnd/okb/Fragment/TraceFriendFragment.java
+++ app/src/main/java/com/playnd/okb/Fragment/TraceFriendFragment.java
@@ -1,29 +1,47 @@
 package com.playnd.okb.Fragment;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.playnd.okb.R;
 
 /**
  * Created by ByeongKwan on 2016-07-18.
  */
-public class TraceFriendFragment extends Fragment {
-    private GoogleMap googleMap;
+public class TraceFriendFragment extends Fragment implements OnMapReadyCallback {
+    private static String TAG = TraceFriendFragment.class.getName();
 
+    GoogleMap googleMap;
     View view;
+    MapView googleMapView;
+
+    static final LatLng tmp_position = new LatLng(37.56, 126.97);
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
     }
 
     @Override
@@ -34,18 +52,83 @@ public class TraceFriendFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_tracefriend_main, container, false);
+        /*try{
+            view = inflater.inflate(R.layout.fragment_tracefriend_main, container, false);
 
-        MapView mapView = (MapView) view.findViewById(R.id.googleMap);
-        mapView.onCreate(savedInstanceState);
-        mapView.onResume();
+            MapsInitializer.initialize(this.getActivity());
 
-        try{
-            MapsInitializer.initialize(getActivity().getApplicationContext());
+            googleMapView = (MapView) view.findViewById(R.id.googleMap);
+            googleMapView.onCreate(savedInstanceState);
+            googleMapView.onResume();
+
+        }catch (InflateException e){
+            Log.e(TAG, "Inflate exception");
+            e.printStackTrace();
         }catch (Exception e){
             e.printStackTrace();
-        }
+        }*/
+        view = inflater.inflate(R.layout.fragment_tracefriend_main, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googleMap);
+
+        mapFragment.getMapAsync(this);
 
         return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Log.d(TAG, "onMapReady");
+        googleMap.setMyLocationEnabled(true);
+
+        Marker seoul = googleMap.addMarker(new MarkerOptions().position(tmp_position).title("테스트!!!"));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tmp_position, 20));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+        googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener(){
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                LatLng location_center = cameraPosition.target;
+                Log.d(TAG, location_center+"");
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        googleMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        googleMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        googleMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        googleMapView.onLowMemory();
     }
 }
